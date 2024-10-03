c4d29241e74999b5d2ff45f94d750cfce56838af
Elias Faith <elias.faith@gmail.com>
2024-10-03 04:07:20 +0100

    I created different function to carry out different tasks example a function that checks if a number is an even number, a function that if a number is negative etc

---
+++ index.js
@@ -0,0 +1,55 @@
+
+// - Write a function that checks if a string is empty
+function Isstringempty(length){
+    return length==0;
+}
+console.log(Isstringempty(0))
+//- ⁠Implement a function that doubles a number
+function doubling(number){
+    return number*2
+}
+console.log(doubling(7))
+//- ⁠Create a function that checks if a number is zero
+function Iszero(number){
+    return number==0;
+}
+console.log(Iszero(0))
+//- ⁠Create a function that calculates the remainder of division
+function division(dividend, divisor){
+    return dividend%divisor;
+}
+console.log(division(17,5))
+//- ⁠Create a function that converts a string to uppercase
+function UpperCase(string){
+    return string.toUpperCase(); 
+}
+console.log (UpperCase("mine"))
+
+// Implement a function that checks if a number is even
+function isNumberEven(number){
+    return number%2==0;
+}
+console.log(isNumberEven(5))
+// ⁠Write a function that calculates the factorial of a number
+function factorialize(number){
+    return number;
+}
+console.log(factorialize(20))   
+// ⁠Write a function that returns the last character of a string
+function lastCharacter(str){
+    return str.charAt(str.length-1);
+}
+console.log(lastCharacter("today"))
+// Write a function that returns the first element of an array
+  let array = [5,4,3,2,1]
+  function firstElement(array){
+    return array.at(0);
+  }
+  console.log(firstElement([5,4,3,2,1]))
+// ⁠Write a function that checks if a number is negative
+function isNumberNegative(number){
+    return number <0;
+}
+console.log (isNumberNegative(5))
+
+//push last night’s codes and these ones to a git repo and submit the repo link here
